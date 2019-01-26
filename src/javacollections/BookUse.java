d8ad3a9407ff9315c96cb090c78c36fdb6bd015b
David Vera <David Vera@Personal>
2019-01-26 18:36:35 -0500

    Personal assignment of week 2

---
+++ src/javacollections/BookUse.java
@@ -0,0 +1,36 @@
+package javacollections;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David Vera
+ */
+public class BookUse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Book book1=new Book("Be patient", "Jose Alonzo", 2354);
+        Book book2=new Book("Be patient", "Jose Alonzo", 2355);
+        
+        //book1=book2;
+        if(book1.equals(book2)){
+            System.out.println("Is the same book");
+            System.out.println(book1.hashCode());
+            System.out.println(book2.hashCode());
+        }else {
+            System.out.println("Is not the same book");
+            System.out.println(book1.hashCode());
+            System.out.println(book2.hashCode());
+        }
+    }
+    
+}
---
+++ src/javacollections/TestLinkedList2.java
@@ -0,0 +1,80 @@
+package javacollections;
+
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author David Vera
+ */
+public class TestLinkedList2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        LinkedList<String> countries=new LinkedList<>();
+        
+        countries.add("Espain");
+        countries.add("Ecuador");
+        countries.add("Colombia");
+        countries.add("Mexico");
+        countries.add("Peru");
+        
+        LinkedList<String> capitals=new LinkedList<>();
+        
+        capitals.add("Madrid");
+        capitals.add("Quito");
+        capitals.add("Bogota");
+        capitals.add("DF");
+        capitals.add("Lima");
+        
+        //System.out.println(countries);
+        //System.out.println(capitals);
+        
+        
+        ListIterator<String> itcountries=countries.listIterator();
+        ListIterator<String> itcapitals=capitals.listIterator();
+        
+        while(itcapitals.hasNext()){
+        
+            if(itcountries.hasNext()){
+                
+                itcountries.next();
+            }
+            
+            itcountries.add(itcapitals.next());
+        }
+        System.out.println(countries); 
+        
+        itcapitals=capitals.listIterator();
+        
+        while(itcapitals.hasNext()){
+        
+            itcapitals.next();
+            
+            if(itcapitals.hasNext()){
+            
+                itcapitals.next();
+                
+                itcapitals.remove();
+            }
+        }
+        
+        System.out.println(capitals);
+        
+        countries.removeAll(capitals);
+        
+        System.out.println(countries);
+    }
+    
+}
