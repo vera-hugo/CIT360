0c44b66b010a93c00d513234dc2cb307056dae38
David Vera <David Vera@Personal>
2019-02-07 13:46:13 -0500

    hibernate project 1

--- HibernateProject1/src/hibernateproject1/HibernateUtils.java
+++ HibernateProject1/src/hibernateproject1/HibernateUtils.java
@@ -5,46 +5,40 @@
  */
 package hibernateproject1;
 
-import javax.imageio.spi.ServiceRegistry;
-import org.eclipse.persistence.jaxb.dynamic.metadata.Metadata;
-import org.hibernate.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
-/**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
- *
- * @author David Vera
- */
+
+
 public class HibernateUtils {
-
     private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    // Hibernate 5:
-    private static SessionFactory buildSessionFactory() {
+    
+    private static SessionFactory buildSessionFactory()
+    {
         try {
-            // Create the ServiceRegistry from hibernate.cfg.xml
-            ServiceRegistry serviceRegistry = (ServiceRegistry) new StandardServiceRegistryBuilder()//
-                    .configure("hibernate.cfg.xml").build();
-
-            // Create a metadata sources using the specified service registry.
-            Metadata metadata = (Metadata) new MetadataSources((org.hibernate.service.ServiceRegistry) serviceRegistry).getMetadataBuilder().build();
-
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
+                    configure("hibernate.cfg.xml").build();
+            
+            Metadata metadata = new MetadataSources(serviceRegistry).
+                    getMetadataBuilder().build();
+            
             return metadata.getSessionFactoryBuilder().build();
         } catch (Throwable ex) {
-
-            System.err.println("Initial SessionFactory creation failed." + ex);
+            System.err.println("Initial SessionFactory creeation failed." + ex);
             throw new ExceptionInInitializerError(ex);
-        }
-       
+        }   
     }
-
-    public static SessionFactory getSessionFactory() {
+    
+    public static SessionFactory getSessionFactory()
+    {
         return sessionFactory;
     }
-
-    public static void shutdown() {
+    
+    public static void shutdown()
+    {
         getSessionFactory().close();
     }
 }
